fd757527642f7761a173f3d9201f1e51ea3f3c9b
Tanvi Kamath <tanvikamath22@gmail.com>
2025-10-26 01:26:47 +0530

    Add files via upload

--- skills.py
+++ skills.py
@@ -1,27 +1,27 @@
 # skills.py
 
-# This is a sample list. A real-world app would have thousands.
 SKILL_KEYWORDS = [
-    # Programming Languages
+    # --- EXISTING SKILLS ---
     'python', 'java', 'c++', 'c#', 'javascript', 'typescript', 'sql', 'go', 'php', 'ruby', 'swift', 'kotlin',
-
-    # Web Frameworks (Frontend)
     'react', 'angular', 'vue', 'vue.js', 'svelte', 'jquery',
-    
-    # Web Frameworks (Backend)
     'django', 'flask', 'spring', 'spring boot', '.net', 'node.js', 'express.js', 'fastapi', 'rubyonrails',
-
-    # Data Science & ML
     'pandas', 'numpy', 'scipy', 'scikit-learn', 'sklearn', 'tensorflow', 'keras', 'pytorch',
     'machine learning', 'deep learning', 'nlp', 'natural language processing', 'data analysis', 'data visualization',
-
-    # Databases
     'mysql', 'postgresql', 'mongodb', 'redis', 'sqlite', 'oracle', 'microsoft sql server',
-
-    # Cloud & DevOps
     'aws', 'amazon web services', 'azure', 'microsoft azure', 'gcp', 'google cloud platform',
     'docker', 'kubernetes', 'terraform', 'ansible', 'jenkins', 'git', 'ci/cd',
-    
-    # Other
-    'agile', 'scrum', 'jira', 'api', 'rest', 'graphql', 'microservices', 'html', 'css', 'sass'
+    'agile', 'scrum', 'jira', 'api', 'rest', 'graphql', 'microservices', 'html', 'css', 'sass',
+
+    # --- NEW ADDITIONS ---
+    'rust', 'scala', 'perl', 'r', 'dart', # Languages
+    'next.js', 'nuxt.js', 'koa', 'pyspark', 'hadoop', 'kafka', 'rabbitmq', 'redux', 'zustand', 'nginx', 'apache', # Frameworks/Libs
+    'cassandra', 'couchbase', 'neo4j', 'dynamodb', 'firebase realtime database', # Databases
+    'ec2', 's3', 'lambda', 'rds', 'eks', 'heroku', 'vercel', 'netlify', # Cloud Services
+    'grafana', 'prometheus', 'datadog', 'splunk', 'elk stack', 'chef', 'puppet', # DevOps/Infra
+    'jest', 'cypress', 'selenium', 'pytest', 'junit', # Testing
+    'confluence', 'trello', 'asana', 'waterfall', 'kanban', # Software/Methodologies
+    'regression', 'classification', 'clustering', 'reinforcement learning', 'computer vision', 'time series analysis', # Data Science
+    'figma', 'sketch', 'adobe xd', # Design
+    'communication', 'leadership', 'teamwork', 'problem-solving' # Soft Skills (use with caution)
+
 ]
